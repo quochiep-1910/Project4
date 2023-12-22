8e0465e52b213b792ff30e688d2f786fad8680a6
quochiep <quochiep1019@gmail.com>
2023-12-22 14:12:22 +0700

    Add init

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -275,7 +275,7 @@ resource "aws_codebuild_project" "codebuild" {
 
   source {
     type            = "GITHUB"
-    location        = "https://github.com/your-org/your-repo"
+    location        = "https://github.com/quochiep-1910/Project4"
     git_clone_depth = 1
     buildspec       = "buildspec.yml"
   }
